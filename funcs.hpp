46648b6e30ed1cba17e4b6d6b0a22ad57f434223
Guilherme Santos <guilherme.ssilva@protonmail.com>
2022-10-09 12:05:21 -0300

    Header file created

---
+++ funcs.hpp
@@ -0,0 +1,14 @@
+#ifndef FUNCS_HPP_INCLUDED
+#define FUNCS_HPP_INCLUDED
+
+void run();
+
+void showMenu();
+
+void showSaldo();
+
+void makeDeposit();
+
+void withdraw();
+
+#endif // FUNCS_HPP_INCLUDED
